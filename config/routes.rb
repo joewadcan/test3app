705adb4fe31b606cb6d9472b4f962a986d5757fd
joewadcan <joe@wadcan.com>
2020-10-19 13:34:01 -0700

    addd a new home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/help'
   resources :posts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root 'application#hello'
+root 'static_pages#home'
 end
